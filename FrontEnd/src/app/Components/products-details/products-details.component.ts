9e7544e2ee630ae54b5535499524798488c0bd8e
Samar Samy <samarsamy4844@gmail.com>
2023-05-04 00:07:38 +0300

    frontend

---
+++ FrontEnd/src/app/Components/products-details/products-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-details',
+  templateUrl: './products-details.component.html',
+  styleUrls: ['./products-details.component.css']
+})
+export class ProductsDetailsComponent {
+
+}
---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ProductsDetailsComponent } from './Components/products-details/products-details.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { AddToCartComponent } from './Components/add-to-cart/add-to-cart.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ErrorComponent } from './Components/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    ProductsDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddToCartComponent,
+    HomeComponent,
+    AboutComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
