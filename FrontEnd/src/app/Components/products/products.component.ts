05ac989e00520556087803335cba25ccc68f1895
Samar Samy <samarsamy4844@gmail.com>
2023-05-05 17:00:32 +0300

    filter dependence on college

--- FrontEnd/src/app/Components/products/products.component.ts
+++ FrontEnd/src/app/Components/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component , OnInit} from '@angular/core';
 import { ProductsService } from 'src/app/Services/products.service';
+import { Product,ProductsData} from './product.interface';
+
 
 @Component({
   selector: 'app-products',
@@ -7,17 +9,33 @@ import { ProductsService } from 'src/app/Services/products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products:any;
+  products: Product[] = [];
+  allProducts: Product[] = [];
+  // products:any;
+  selectedCollege: string = 'all';
   constructor(public myService:ProductsService){ }
   ngOnInit(): void {
-    this.myService.GetAllProducts().subscribe(
-      {
-        next:(data)=>{
-          this.products = data;
-        },
-        error:(err)=>{console.log(err)}
+    this.selectedCollege="all";
+    this.myService.GetAllProducts().subscribe({
+      next: (data: any) => {
+        this.allProducts = data;
+        this.products = this.allProducts;
+      },
+      error: (err) => {
+        console.log(err);
       }
-    )
+    });
   }
 
+  filterProductsByCollege(college: string) {
+    this.selectedCollege = college;
+    if (  college  === 'all') {
+         // if 'all' is selected, show all products
+      this.products = this.allProducts;
+    } else {
+       // filter products based on selected college
+      if(this.allProducts){
+      this.products = this.allProducts.filter(p => p.college.toLowerCase() === college.toLowerCase());
+    }}
+  }
 }
--- FrontEnd/src/app/Services/products.service.ts
+++ FrontEnd/src/app/Services/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductsData, Product } from '../Components/products/product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,3 +16,4 @@ export class ProductsService {
   }
 
 }
+
