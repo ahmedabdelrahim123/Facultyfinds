69e43b0ce09ed9025ce489fba551db6d4ee15963
wafaamuhammad123 <wafaa.muhammad2000@gmail.com>
2023-05-06 21:57:47 +0300

    add to cart

--- FrontEnd/src/app/Components/add-to-cart/add-to-cart.component.ts
+++ FrontEnd/src/app/Components/add-to-cart/add-to-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/Services/cart.service';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AddToCartComponent {
 
+  public products : any = [];
+  public grandTotal !: number;
+  public totalItem : number = 0;
+
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+    // When the observable emits a value, the products property of the component is set to the emitted value,
+    //  and the grandTotal property is set to the total price of all items in the cart, calculated by 
+    //  calling the getTotalPrice method of the CartService.
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+        this.totalItem = res.length;
+      
+    })
+  }
+  removeItem(item: any){
+    //It calls the removeCartItem method of the CartService with the item to be removed as an argument.
+    this.cartService.removeCartItem(item);
+  }
+  emptycart(){
+    //remove all items from the cart. It calls the removeAllCart method of the CartService.
+    this.cartService.removeAllCart();
+  }
+
+//   quantity(value:string){
+//     if(this.item.quantity <8 && value=="max"){
+//       this.item.quantity +=1;
+//     }
+//     else   if(this.item.quantity>1 && value=="min"){
+//       this.item.quantity -=1;
+//   }
+// }
+ 
 }
---
+++ FrontEnd/src/app/Services/api.services.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+  getProduct(){//use httpclient to make get req to this link to get data 
+    return this.http.get<any>("https://fakestoreapi.com/products")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+}
+// The pipe() method is then used to apply one or more RxJS operators to the Observable
+// The map operator is used to transform the response data to a new format.
+// here the map operator is passed a function that takes the response data (res) as an argument and returns it as is.
+//  This means that the response data is not transformed in any way,  and the method returns an // Observable that emits the 
+// response data as an any type.
