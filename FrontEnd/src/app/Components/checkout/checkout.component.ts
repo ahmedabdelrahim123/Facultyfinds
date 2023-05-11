6af98c9adc381692f33c8557121a40c3cde55d5c
wafaamuhammad123 <wafaa.muhammad2000@gmail.com>
2023-05-11 23:36:15 +0300

    checkout

--- FrontEnd/src/app/Components/checkout/checkout.component.ts
+++ FrontEnd/src/app/Components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// import { Validators } from '@angular/forms';
+// import { FormGroup, FormBuilder } from '@angular/forms';
 import { CartService } from 'src/app/Services/cart.service';
 @Component({
   selector: 'app-checkout',
@@ -7,7 +8,7 @@ import { CartService } from 'src/app/Services/cart.service';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-
+  // myForm: FormGroup;
   public products : any = [];
   public grandTotal !: number;
   public Total !: number;
@@ -15,9 +16,14 @@ export class CheckoutComponent {
   // item: any;
 
 
-constructor(private cartService : CartService) { }
+constructor(private cartService : CartService) {
+  // this.myForm = this.fb.group({
+  //   name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]]
+  // });
+ }
 
 ngOnInit(): void {
+
 // When the observable emits a value, the products property of the component is set to the emitted value,
 //  and the grandTotal property is set to the total price of all items in the cart, calculated by 
 //  calling the getTotalPrice method of the CartService.
