6b3f6f1ce8d721ec672d861af1908a6c4cd2dde1
Samar Samy <samarsamy4844@gmail.com>
2023-05-13 12:33:47 +0300

    routes

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { ProductsDetailsComponent } from './Components/products-details/products
 import { ProductsComponent } from './Components/products/products.component';
 import { CartComponent } from './Components/cart/cart.component';
 import { CheckoutComponent } from './Components/checkout/checkout.component';
+import { HomeComponent } from './Components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component:ProductsComponent},
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
   {path:'products', component:ProductsComponent},
   {path:'products/:id', component:ProductsDetailsComponent},
+  // {path:'addproducts', component:Add},
   {path:'cart', component: CartComponent},
   {path:'checkout', component: CheckoutComponent}
-  
 ];
 
 @NgModule({
