61791bf706a7fe8bcbff786404b5ef7988c05e01
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-05-13 08:43:11 +0300

    .

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsDetailsComponent } from './Components/products-details/products-details.component';
 import { ProductsComponent } from './Components/products/products.component';
+import { HomeComponent } from './Components/home/home.component';
 
 const routes: Routes = [
   {path:'', component:ProductsComponent},
   {path:'products', component:ProductsComponent},
   {path:'products/:id', component:ProductsDetailsComponent},
-  
+  {path:'Home', component:HomeComponent},
 ];
 
 @NgModule({
