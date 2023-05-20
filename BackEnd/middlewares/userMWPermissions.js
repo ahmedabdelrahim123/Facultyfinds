7304dd1dba82bcc09b8e02782a658861ed6aa6fc
Samir <ahmedsamir25999@gmail.com>
2023-05-20 14:00:45 +0300

    authen

--- BackEnd/middlewares/userMWPermissions.js
+++ BackEnd/middlewares/userMWPermissions.js
@@ -3,13 +3,13 @@ var jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   var token = req.header("x-auth-token");
   console.log(token);
-  if (!token) return res.status(400).send("login first please...");
+  if (!token) return res.status(401).send("login first please...");
 
   var decodedPayload = jwt.verify(token, "thistokensecret");
   // console.log(decodedPayload);
   if (decodedPayload.userType === "admin") {
     next(); //isAdmin
   } else {
-    res.status(400).send("You are not an admin");
+    res.status(401).send("You are not an admin");
   }
 };
