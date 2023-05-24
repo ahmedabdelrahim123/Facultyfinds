0506fc234053aef9bbf642f0649611d77a691adb
Samar Samy <samarsamy4844@gmail.com>
2023-05-24 14:03:32 +0300

    .

--- BackEnd/Controllers/OrdersController.js
+++ BackEnd/Controllers/OrdersController.js
@@ -4,7 +4,9 @@ const jwt = require("jsonwebtoken");
 
 let getAllOrders = async (req, res) => {
   let data = await ordersModel.find({}).populate('userID', 'username').populate('pID', 'title price');
-  res.json(data);
+  const totalOrdersCount = await ordersModel.countDocuments();
+  const pendingOrders = await ordersModel.find({ statue: 'pending' }).countDocuments();
+  res.json(data, totalOrdersCount, pendingOrders);
 };
 let createOrder = async (req, res) => {
 data=req.body;
--- FrontEnd/src/app/Components/dashborad/dashborad.component.ts
+++ FrontEnd/src/app/Components/dashborad/dashborad.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
 
 @Component({
   selector: 'app-dashborad',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashborad.component.css']
 })
 export class DashboradComponent {
+  orders: any;
+  totalOrdersCount: number=0;
+  pendingOrders: any[]=[];
+  constructor(private api : DataService) { }
+  ngOnInit(): void {
+    this.api.getMyOrders().subscribe(
+      {
+        next:(data)=>{
+          this.orders = data;
+          console.log(this.orders);
+        },
+        error:(err)=>{console.log(err)}
+      }
+    )
+  }
 
 }
