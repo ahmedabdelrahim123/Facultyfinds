6eb836a5e6e8104f2d7781ecb88c7db0c586f259
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-05-25 03:47:27 +0300

    error message in add product

--- FrontEnd/src/app/Components/create-product/create-product.component.ts
+++ FrontEnd/src/app/Components/create-product/create-product.component.ts
@@ -13,7 +13,8 @@ export class CreateProductComponent {
 
   }
   AddProduct(title: any, price: any, details: any, college: any, image: any) {
-    if (image.files && image.files.length > 0) {
+    if (image.files && image.files.length > 0 && title && price && details && college) {
+      (document.getElementById("error-message") as HTMLElement).style.display = "none";
       this.imageFile = image.files[0];
       const formData = new FormData();
       formData.append('image', image.files[0]);
@@ -30,6 +31,9 @@ export class CreateProductComponent {
         (err) => {
         }
       );
+    }
+    else{
+      (document.getElementById("error-message") as HTMLElement).style.display = "block"; // show error message
     }
   }
 }
