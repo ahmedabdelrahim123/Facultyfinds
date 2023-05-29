54ae3856c10f31183a87911df7a36588b2b7c57e
Samar Samy <samarsamy4844@gmail.com>
2023-05-29 12:08:01 +0300

    delay some pages

--- FrontEnd/src/app/Components/contactus/contactus.component.ts
+++ FrontEnd/src/app/Components/contactus/contactus.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent {
+  constructor(public router:Router){}
+  delayedNavigation() {
+    setTimeout(() => {
+      this.router.navigate(['/msgsend']);
+    }, 1000);
+  }
+
 
 }
